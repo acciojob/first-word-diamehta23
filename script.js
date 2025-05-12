ecc7c7fc96540d2c38533201bd6d840282c98377
diamehta23 <154620279+diamehta23@users.noreply.github.com>
2025-05-12 10:52:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstWord(s) {
-  // your code here
+  if (!s) return ""; 
+
+    const spaceIndex = s.indexOf(" ");
+    if (spaceIndex === -1) {
+        return s; 
+    }
+
+    return s.substring(0, spaceIndex);
 }
+console.log(firstWord("see and stop"));
+console.log(firstWord("Hello World"));
+
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
